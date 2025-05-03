475e8ac1a032af0d4cecd94b55887f7e8e71a8a2
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-05-03 22:23:36 +0500

    first

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/api/v1/tasks', (req, res) => {
-
 })
 
 app.post('/api/v1/tasks', (req, res) => {
-    
 })
 
 app.get('/api/v1/tasks/:id', (req, res) => {
@@ -20,11 +18,9 @@ app.get('/api/v1/tasks/:id', (req, res) => {
 })
 
 app.patch('/api/v1/tasks/:id', (req, res) => {
-    
 })
 
 app.delete('/api/v1/tasks/:id', (req, res) => {
-    
 })
 
 
