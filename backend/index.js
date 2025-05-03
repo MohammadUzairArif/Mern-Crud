507dc3a201a34fbe8d6c9bd098c0076f8a6cc674
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-05-03 22:22:22 +0500

    first

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+
+import express from 'express';
+import dotenv from 'dotenv';
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+app.get('/api/v1/tasks', (req, res) => {
+
+})
+
+app.post('/api/v1/tasks', (req, res) => {
+    
+})
+
+app.get('/api/v1/tasks/:id', (req, res) => {
+    const { id } = req.params;
+    res.send(`Get task with id: ${id}`);
+})
+
+app.patch('/api/v1/tasks/:id', (req, res) => {
+    
+})
+
+app.delete('/api/v1/tasks/:id', (req, res) => {
+    
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}` );
+})
