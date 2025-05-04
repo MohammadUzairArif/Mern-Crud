10710217f1bc5174fd6b7edd474f8be235ae69f4
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-05-04 17:17:20 +0500

    database added

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,34 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import Task from './models/task.js';
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
-app.get('/api/v1/tasks', (req, res) => {
+
+app.get('/api/v1/tasks', async (req, res) => {
+    try {
+        const getTasks = await Task.find({})
+        res.status(200).json({ getTasks });
+        
+    } catch (error) {
+        console.error("Error fetching tasks:", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 })
 
-app.post('/api/v1/tasks', (req, res) => {
+app.post('/api/v1/tasks', async (req, res) => {
+    const { title, completed } = req.body;
+    try {
+        const task = await Task.create({ title, completed });
+        res.status(201).json({ task });
+    } catch (error) {
+        console.error("Error creating task:", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 })
 
 app.get('/api/v1/tasks/:id', (req, res) => {
@@ -23,8 +42,9 @@ app.patch('/api/v1/tasks/:id', (req, res) => {
 app.delete('/api/v1/tasks/:id', (req, res) => {
 })
 
-
+dotenv.config();
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is running on port ${PORT}` );
 })
